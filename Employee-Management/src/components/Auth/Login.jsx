75683ed918c4ea37848ba8b0805b8f5b635a49ff
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-10-21 19:02:09 +0000

    Login

--- Employee-Management/src/components/Auth/Login.jsx
+++ Employee-Management/src/components/Auth/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
         <form className='flex flex-col items-center justify-center'>
           <input className='border-2 border-red-600' type="email" placeholder="Enter your mail" />
           <input className='border-2' type="password" placeholder="Enter your password" />
-          <button className=''>LogIn</button>
+          <button className=''>Login</button>
         </form>
       </div>
     </div>
