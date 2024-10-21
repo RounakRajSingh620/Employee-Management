30d9dfd1ac2e84f4f3d877f1f0441509d948d5de
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-10-21 18:20:20 +0000

    Login

--- Employee-Management/src/components/Auth/Login.jsx
+++ Employee-Management/src/components/Auth/Login.jsx
@@ -4,8 +4,9 @@ const Login=()=>{
   return (
     <div className='flex items-center justify-center'>
       <div className='border-2 border-red-600'>
-        <form>
-          <input type="email"/>
+        <form className=''>
+          <input type="email" placeholder="Enter your mail"/>
+          <input type="password" placeholder="Enter your password"/>
         </form>
       </div>
       </div>
