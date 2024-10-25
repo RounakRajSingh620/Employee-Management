8f5a1f5c1ed3a820a6652d368fc23434519b73a1
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-10-25 13:50:44 +0000

    Login Setup

--- Employee-Management/src/components/Auth/Login.jsx
+++ Employee-Management/src/components/Auth/Login.jsx
@@ -6,9 +6,10 @@ const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const submitHandler = (e) => { 
+  const submitHandler = (e) => {
     e.preventDefault()
-    console.log("heloo guys,form submitted")
+    console.log("email is", email)
+    console.log("password is ", password)
   }
 
   return (
@@ -17,10 +18,14 @@ const Login = () => {
         <form onSubmit={(e) => { submitHandler(e) }} className='flex flex-col items-center justify-center'>
           <input value={email} onChange={
             (e) => {
-              console.log(e.target.value)
+              setEmail(e.target.value)
             }
           } required className=' outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400 ' type="email" placeholder="Enter your gmail" />
-          <input required className=' outline-none bg-transparent border-2 border-emerald-600 mt-3 text-xl py-3 px-5 rounded-full placeholder:text-gray-400 ' type="password" placeholder="Enter your password" />
+          <input value={password} onChange={(e) => {
+            setPassword(e.target.value)
+          }
+          } required className=' outline-none bg-transparent border-2 border-emerald-600 mt-3 text-xl py-3 px-5 rounded-full placeholder:text-gray-400 ' type="password" placeholder="Enter your password" />
+
           <button className='text-white border-none outline-none bg-emerald-600 mt-5 text-xl py-3 px-5 rounded-full placeholder:text-white '>Login</button>
         </form>
       </div>
