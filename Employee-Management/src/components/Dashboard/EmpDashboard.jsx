6f0071c6ee5eda32d9a04d666a19b18cceff27f0
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-10-26 16:25:10 +0000

    Employee_Dashboard

--- Employee-Management/src/components/Dashboard/EmpDashboard.jsx
+++ Employee-Management/src/components/Dashboard/EmpDashboard.jsx
@@ -3,10 +3,10 @@ import Header from "../Misc/Header"
 
 const EmployeeDashboard = () => {
     return (
-        <div>
+        <div className="p-10 bg-[#1C1C1C] h-screen">
             <Header />
         </div>
-    )
+    ) 
 }
 
 export default EmployeeDashboard
--- Employee-Management/src/components/Misc/Header.jsx
+++ Employee-Management/src/components/Misc/Header.jsx
@@ -2,8 +2,11 @@
 const Header = () => {
     return (
         <div className="flex items-end justify-between ">
-            <h1>Hello <br /> Raunak✌️ </h1>
-            <button>Log Out</button>
+            <h1 className="text-2xl font-medium"> Hello,
+                <br />
+                <span className="text-3xl font-semibold">Raunak✌️</span>
+            </h1>
+            <button className="px-5 py-2 text-lg text-white bg-red-600 rounded-sm">Log Out</button>
         </div>
     )
 }
